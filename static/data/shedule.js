9828ca451993f671d9e43658f5355768c774fca3
danil <ap.fomin203@gmail.com>
2024-09-08 17:20:38 +0300

    origin main

---
+++ static/data/shedule.js
@@ -0,0 +1,1220 @@
+export const schedule = {
+	robotics: [
+		{
+			lessonTime: "09:00 - 09:45",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "НПК МР-1б",
+					cabinet: "11",
+					teacher: "Городничев Д.В",
+				},
+				{
+					group: "НПК ИСП 1а",
+					cabinet: "11",
+					teacher: "Городничев Д.В",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "09:55 - 10:40",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "НПК МР-1б",
+					cabinet: "11",
+					teacher: "Городничев Д.В",
+				},
+				{
+					group: "НПК ИСП 1а",
+					cabinet: "11",
+					teacher: "Городничев Д.В",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "10:50 - 11:35",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "НПК МР-1а",
+					cabinet: "11",
+					teacher: "Городничев Д.В",
+				},
+				{
+					group: "НПК ИСП 1б",
+					cabinet: "11",
+					teacher: "Городничев Д.В",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "11:00 - 11:45",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "ТЛ-3-1",
+					cabinet: "11",
+					teacher: "Городничев Д.В",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "11:45 - 12:30",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "НПК МР-1а",
+					cabinet: "11",
+					teacher: "Городничев Д.В",
+				},
+				{
+					group: "НПК ИСП 1б",
+					cabinet: "11",
+					teacher: "Городничев Д.В",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "11:55 - 12:40",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "ТЛ-3-1",
+					cabinet: "11",
+					teacher: "Городничев Д.В",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "15:20 - 16:05",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "ТЛ-3-1",
+					cabinet: "11",
+					teacher: "Городничев Д.В",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "16:15 - 17:00",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "ТЛ-3-1",
+					cabinet: "11",
+					teacher: "Городничев Д.В",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+	],
+	python: [
+		{
+			lessonTime: "13:15 - 14:00",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "ЗП-1-2",
+					cabinet: "13",
+					teacher: "Аксёнов Р.В.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "14:10 - 14:55",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "ЗП-1-2",
+					cabinet: "13",
+					teacher: "Аксёнов Р.В.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "15:05 - 15:50",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "ЗП-1-1а",
+					cabinet: "13",
+					teacher: "Аксёнов Р.В.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "16:00 - 16:45",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "ЗП-1-1а",
+					cabinet: "13",
+					teacher: "Аксёнов Р.В.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "16:55 - 17:40",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "ЗП-1-1б",
+					cabinet: "13",
+					teacher: "Аксёнов Р.В.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "17:10 - 17:55",
+			days: [
+				{
+					group: "ЗП-1-2",
+					cabinet: "13",
+					teacher: "Аксёнов Р.В.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "17:50 - 18:35",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "ЗП-1-1б",
+					cabinet: "13",
+					teacher: "Аксёнов Р.В.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "18:05 - 18:50",
+			days: [
+				{
+					group: "ЗП-1-2",
+					cabinet: "13",
+					teacher: "Аксёнов Р.В.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+	],
+	web: [
+		{
+			lessonTime: "12:50 - 13:35",
+			days: [
+				{
+					group: "ВР-5-6б",
+					cabinet: 23,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "13:45 - 14:30",
+			days: [
+				{
+					group: "ВР-5-6б",
+					cabinet: 23,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "14:40 - 15:25",
+			days: [
+				{
+					group: "ВР-5-8а",
+					cabinet: 23,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "ПС-3-1а",
+					cabinet: 24,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "15:35 - 16:20",
+			days: [
+				{
+					group: "ВР-5-8а",
+					cabinet: 23,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "ПС-3-1а",
+					cabinet: 24,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "16:30 - 17:15",
+			days: [
+				{
+					group: "ПС-3-1а",
+					cabinet: 23,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "ПС-3-1в",
+					cabinet: 24,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: "ПС-3-1б",
+					cabinet: 24,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "17:25 - 18:10",
+			days: [
+				{
+					group: "ПС-3-1а",
+					cabinet: 23,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "ПС-3-1в",
+					cabinet: 24,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: "ПС-3-1б",
+					cabinet: 24,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "18:20 - 19:05",
+			days: [
+				{
+					group: "ПС-3-1б",
+					cabinet: 23,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "ВР-2-1",
+					cabinet: 24,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: "ПС-3-1в",
+					cabinet: 24,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "19:15 - 20:00",
+			days: [
+				{
+					group: "ПС-3-1б",
+					cabinet: 23,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "ВР-2-1",
+					cabinet: 24,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: "ПС-3-1в",
+					cabinet: 24,
+					teacher: "Фомин Д.А.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+	],
+	mobile: [
+		{
+			lessonTime: "14:50 - 15:35",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "МП-1-1а",
+					cabinet: "23",
+					teacher: "Андреев Е.Р.",
+				},
+				{
+					group: "МП-1-1а",
+					cabinet: "23",
+					teacher: "Андреев Е.Р.",
+				},
+			],
+		},
+		{
+			lessonTime: "15:45 - 16:30",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "МП-1-1а",
+					cabinet: "23",
+					teacher: "Андреев Е.Р.",
+				},
+				{
+					group: "МП-1-1а",
+					cabinet: "23",
+					teacher: "Андреев Е.Р.",
+				},
+			],
+		},
+		{
+			lessonTime: "16:40 - 17:25",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "МП-1-1б",
+					cabinet: "23",
+					teacher: "Андреев Е.Р.",
+				},
+				{
+					group: "МП-1-1б",
+					cabinet: "23",
+					teacher: "Андреев Е.Р.",
+				},
+			],
+		},
+		{
+			lessonTime: "17:35 - 18:20",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "МП-1-1б",
+					cabinet: "23",
+					teacher: "Андреев Е.Р.",
+				},
+				{
+					group: "МП-1-1б",
+					cabinet: "23",
+					teacher: "Андреев Е.Р.",
+				},
+			],
+		},
+	],
+	java: [
+		{
+			lessonTime: "13:00 - 13:45",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "Java-1-1",
+					cabinet: "23",
+					teacher: "Андреев Е.Р.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+		{
+			lessonTime: "13:55 - 14:40",
+			days: [
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+				{
+					group: "Java-1-1",
+					cabinet: "23",
+					teacher: "Андреев Е.Р.",
+				},
+				{
+					group: null,
+					cabinet: null,
+					teacher: null,
+				},
+			],
+		},
+	],
+};
